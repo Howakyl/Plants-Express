550b3050f213c77f0837d00efdfb7895a39a5455
Howakyl96 <howakyl@gmail.com>
2020-10-01 15:36:10 -0700

    commit 4 - plant profile showing plant object info

--- controllers/plantsControllers.js
+++ controllers/plantsControllers.js
@@ -3,11 +3,28 @@ const router = express.Router();
 
 const plants = require ('../models/Plant');
 
+//Plant HOME Index
 router.get('/' , (req,res) => {
     res.render('plants/indexPlant' , {
         plants: plants
     });
     // res.send('is it working??');
+});
+
+//Plant INFO 
+router.get('/:plantIndex' , (req,res) => {
+    const plantIndex = req.params.plantIndex;
+    const plant = plants[plantIndex];
+
+    if(plants[plantIndex]) {
+        res.render('plants/showPlantInfo' , {
+            plants: plant,
+        });
+    } else {
+        res.render('plants/showPlantInfo',  {
+            plant: {name: 'Does not exist'},
+        });
+    }
 });
 
 module.exports = router;
